7af28e31b894432fe653f51d551ade4e937ade9e
Ayush Mall <ayushmall124@gmail.com>
2020-06-15 09:21:57 +0530

    small change to the library

---
+++ build/precache-manifest.530bc29e14ec0b667a3d70a74b03b24a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ed8c41afc21ab766cffcb7b39f768f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ba33580ab9281ac482e",
+    "url": "/static/js/2.e57cdfd4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e57cdfd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829ae23fa2ad086452ef",
+    "url": "/static/js/main.73491dff.chunk.js"
+  },
+  {
+    "revision": "a21540cf85fa4a046d8f",
+    "url": "/static/js/runtime-main.f1b437b1.js"
+  }
+]);
